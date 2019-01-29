99a4d9ad1d014f51f0e93f0f191219a63453c16e
arnvijay <arnvijay@ARNVIJAY-LAP.oradev.oraclecorp.com>
2019-01-29 19:45:31 +0530

    Added Draw, Win highlight, Current Selection Highlight Feature

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
       return (
-        <button className="square" onClick={props.onClick}>
+        <button className={props.winnerClass} onClick={props.onClick1}>
           {props.value}
         </button> 
       );
@@ -12,13 +12,17 @@ function Square(props) {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+      var myClass2 = 'square';
+      if(this.props.winnerRow[0] === i || this.props.winnerRow[1] === i || this.props.winnerRow[2] === i) {
+        myClass2 = 'winner-square'
+      }
+      return <Square value={this.props.squares[i]} winnerClass={myClass2} onClick1={() => this.props.onClick(i)} />;
     }
-    
+
     render() {
       return (
         <div>
-            <div className="board-row">
+          <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
@@ -46,11 +50,14 @@ function Square(props) {
                     squares: Array(9).fill(null)
                 }],
                 xIsNext:true,
-                stepNumber: 0
+                stepNumber: 0,
+                currentPosition: null,
+                historyPos: [0]
             };
       }
       handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber+1);
+        const historyPos1 = this.state.historyPos.slice(0, this.state.stepNumber+1);
         const current = history[history.length-1];
         const sq = current.squares.slice();
         if(calcWinner(sq) || sq[i]) {
@@ -62,7 +69,9 @@ function Square(props) {
                 squares: sq
             }]),
             stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
+            currentPosition: i,
+            historyPos: historyPos1.concat([i])
         });
     }
     jumpTo(step) {
@@ -75,22 +84,28 @@ function Square(props) {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
         const status = calcWinner(current.squares) ? 
-        'Winner is '+calcWinner(current.squares) :
-        'Next player: '+(this.state.xIsNext ? 'X' : 'O');
+        'Winner is '+calcWinner(current.squares)['player1'] : (this.state.stepNumber === 9 ? 'Its a Draw' : 'Next player: '+(this.state.xIsNext ? 'X' : 'O'));
+        
+        const className1 = 'button-highlight';
+
 
         const moves = history.map((step, move) => {
-            const desc = move ? 'Go to move '+move : 'Go to start';
+            const desc = move ? 'Go to move '+move+' [row: '+(Math.floor(this.state.historyPos[move]/3)+1)+' col: '+((this.state.historyPos[move]%3)+1)+']' : 'Go to start';
+            const myClass = this.state.stepNumber === move ? className1 : '';
             return (
                 <li>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button className={myClass} onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
         });
-   
+      let myWinnerRow = [];
+      if(calcWinner(current.squares)) {
+          myWinnerRow = calcWinner(current.squares)['winIndex'];
+      }
       return (
         <div className="game">
           <div className="game-board">
-            <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
+            <Board squares={current.squares} winnerRow={myWinnerRow} onClick={(i) => this.handleClick(i)}/>
           </div>
           <div className="game-info">
             <div>{status}</div>
@@ -122,7 +137,10 @@ function Square(props) {
       for(let i = 0; i < lines.length; i++) {
           const [a, b, c] = lines[i];
           if(s[a] === s[b] && s[a] === s[c] && s[a]) {
-              return s[a];
+              return {
+                      player1: s[a],
+                      winIndex: lines[i]
+                     }; 
           }
       }
       return null;
